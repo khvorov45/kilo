7653c9bebc9034595df9602f206365481a5a67ef
Arseniy Khvorov <khvorov45@gmail.com>
2021-09-27 13:01:48 +1000

    enable raw mode

--- code/kilo.c
+++ code/kilo.c
@@ -1,4 +1,40 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= CS8;
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
-    int x = 0;
+    enableRawMode();
+
+    char ch;
+    while (read(STDIN_FILENO, &ch, 1) == 1 && ch != 'q') {
+        if (iscntrl(ch)) {
+            printf("%d\r\n", ch);
+        } else {
+            printf("%d, ('%c')\r\n", ch, ch);
+        }
+    }
+
     return 0;
 }
