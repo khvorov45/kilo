7887e6f14e050b1de2d7a4e620c3cd14e28ae1f4
Arseniy Khvorov <khvorov45@gmail.com>
2021-09-28 18:04:52 +1000

    read window size

--- code/kilo.c
+++ code/kilo.c
@@ -2,12 +2,15 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 struct editorConfig {
+    int screen_rows;
+    int screen_cols;
     struct termios orig_termios;
 };
 
@@ -48,6 +51,17 @@ void enableRawMode() {
     }
 }
 
+int getWindowSize(int* rows, int* cols) {
+    struct winsize ws;
+    int result = -1;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) != -1 && ws.ws_col != 0) {
+        *rows = ws.ws_row;
+        *cols = ws.ws_col;
+        result = 0;
+    }
+    return result;
+}
+
 char editorReadKey() {
     int nread;
     char ch;
@@ -69,7 +83,7 @@ void editorProcessKeyPress() {
 }
 
 void editorDrawRows() {
-    for (int row_index = 0; row_index < 24; row_index++) {
+    for (int row_index = 0; row_index < Editor.screen_rows; row_index++) {
         write(STDOUT_FILENO, "~\r\n", 3);
     }
 }
@@ -83,8 +97,15 @@ void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
+void initEditor() {
+    if (getWindowSize(&Editor.screen_rows, &Editor.screen_cols) == -1) {
+        die("getWindowSize");
+    }
+}
+
 int main() {
     enableRawMode();
+    initEditor();
 
     for (;;) {
         editorRefreshScreen();
