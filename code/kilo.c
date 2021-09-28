d6d2f6d7c2f0cf012f246f6f9d460ff7775567ad
Arseniy Khvorov <khvorov45@gmail.com>
2021-09-28 17:55:04 +1000

    draw rows

--- code/kilo.c
+++ code/kilo.c
@@ -64,8 +64,18 @@ void editorProcessKeyPress() {
     }
 }
 
+void editorDrawRows() {
+    for (int y = 0; y < 24; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
